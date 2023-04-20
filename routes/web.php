83b9ee0bd9f2e089058b6abaf62e0e3506a08420
verinu8 <louve.scr8@gmail.com>
2023-04-20 14:57:33 +0900

    8-3課題2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/posts',[PostController::class,'index']);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PostController::class,'index']);
+Route::get('/posts/{post}',[PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetのリクエストが来たら、PostControllerのshowメソッドを実行する。
+
