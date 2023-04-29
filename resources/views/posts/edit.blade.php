fdb7c201a36a1abb493078788980e09290a9e358
verinu8 <louve.scr8@gmail.com>
2023-04-29 15:02:16 +0900

    8-5

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,24 +5,21 @@
         <title>Blog</title>
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <form action="/posts{{$post->id}}" method="POST">
+<h1 class="title">編集画面</h1>
+    <div class="content">
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{$post->title}}"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            <div class='content__title'>
+                <h2>タイトル</h2>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
             </div>
-            <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{$post->body) }}</textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <input type='text' name='post[body]' value="{{ $post->body }}">
             </div>
-            <input type="submit" value="update"/>
+            <input type="submit" value="保存">
         </form>
-        <div class="back">
-            <a href="/posts/{{$post->id}}">back</a>
-        </div>
-    </body>
+    </div>
+</body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,17 +7,20 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1 class='Title'>
-            {{$post->Title}}
-        </h1>
-        <div class='content'>
-                <div class='content_post'>
-                    <h3>本文</h3>
+        <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="/posts/{{$post->id}}">{{$post->title}}</a>
+                    </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
+            @endforeach
         </div>
-        <div class='footer'>
-            <a href="/">戻る</a>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
